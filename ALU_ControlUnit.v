998a46c5967c0f97792f9d7cd61a1d54cf11a620
WongKe <wong.ke@husky.neu.edu>
2017-04-24 18:54:01 -0400

    Update ALU_ControlUnit.v

--- ALU_ControlUnit.v
+++ ALU_ControlUnit.v
@@ -10,16 +10,16 @@
 
 `timescale 1ns / 1ns
 
-module ALU_ControlUnit(ALUop, addi, func, ALUoperation);
+module ALU_ControlUnit(ALUop, addi, func, ALUctrlop);
   
   input [1:0] ALUop;
   input addi;
   input [5:0] func;
-  output reg [3:0] ALUoperation;
+  output reg [3:0] ALUctrlop;
   
   // initialize
   initial begin
-    ALUoperation = 4'b0000;
+    ALUctrlop = 4'b0000;
   end
   
   always @ (ALUop or func) begin
@@ -27,29 +27,29 @@ module ALU_ControlUnit(ALUop, addi, func, ALUoperation);
     case (ALUop)
       
       // lw & sw (add)
-      2'b00: begin ALUoperation = 4'b0010; end
+      2'b00: begin ALUctrlop = 4'b0010; end
       
       // beq (sub)
-      2'b01: begin ALUoperation = 4'b0110; end
+      2'b01: begin ALUctrlop = 4'b0110; end
       
       // ALUOp1 == 1
       2'b10: begin
         // allow for addi
         case (addi)
-          1'b1: begin ALUoperation = 4'b0010; end
+          1'b1: begin ALUctrlop = 4'b0010; end
           
           1'b0: begin
             case (func[3:0]) // func[5:4] is don't care
               // add
-              4'b0000: begin ALUoperation = 4'b0010; end
+              4'b0000: begin ALUctrlop = 4'b0010; end
               // sub
-              4'b0010: begin ALUoperation = 4'b0110; end
+              4'b0010: begin ALUctrlop = 4'b0110; end
               // and
-              4'b0100: begin ALUoperation = 4'b0000; end
+              4'b0100: begin ALUctrlop = 4'b0000; end
               // or
-              4'b0101: begin ALUoperation = 4'b0001; end
+              4'b0101: begin ALUctrlop = 4'b0001; end
               // slt
-              4'b1010: begin ALUoperation = 4'b0111; end
+              4'b1010: begin ALUctrlop = 4'b0111; end
             endcase
           end
         endcase
@@ -59,9 +59,9 @@ module ALU_ControlUnit(ALUop, addi, func, ALUoperation);
       2'b11: begin
         case (func[3:0]) // func[5:4] is don't care
           // sub
-          4'b0010: begin ALUoperation = 4'b0110; end
+          4'b0010: begin ALUctrlop = 4'b0110; end
           // slt
-          4'b1010: begin ALUoperation = 4'b0111; end
+          4'b1010: begin ALUctrlop = 4'b0111; end
         endcase
       end
     endcase
