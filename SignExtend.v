074b94422e7e688f0e33356e5fda1d5ffb271598
WongKe <wong.ke@husky.neu.edu>
2017-04-24 18:44:10 -0400

    Update SignExtend.v

--- SignExtend.v
+++ SignExtend.v
@@ -5,14 +5,14 @@
 
 // Sign Extend module
 
-module SignExtend(instr, se_out);
+module SignExtend(instr, SEout);
   
   input [15:0] instr;       // Instruction [15-0]
-  output reg [31:0] se_out; // Sign extend output [31-0]
+  output reg [31:0] SEout; // Sign extend output [31-0]
   
   // extend the MSB
   always @ (instr) begin
-    se_out <= $signed(instr);
+    SEout <= $signed(instr);
   end
   
 endmodule
