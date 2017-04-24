71904d6cbea8fb5f79d30c24bead2035e3ef958a
WongKe <wong.ke@husky.neu.edu>
2017-04-24 18:36:39 -0400

    Update 2to1mux.v

--- 2to1mux.v
+++ 2to1mux.v
@@ -7,25 +7,25 @@
 
 `timescale 1ns / 1ns
 
-module mux2to1(i0, i1, sel, out);
+module mux2to1(in0, in1, sel, out);
   
-  input [31:0] i0, i1;    // input 1 and 2
+  input [31:0] in0, in1;    // input 1 and 2
   input sel;              // select bit
   output reg [31:0] out;  // output
   
   // loop for mux operation
-  always @ (sel or i0 or i1)
+  always @ (sel or in0 or in1)
   begin
     case(sel)
       // if sel is 0...
       1'b0: begin
         // select input 1
-        out <= i0;
+        out <= in0;
       end
       // if sel is 1...
       1'b1: begin
         // select input 2
-        out <= i1;
+        out <= in1;
       end
     endcase
   end
