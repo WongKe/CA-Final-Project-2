6924ce02aa17d926824508e6254ad35ff653ec17
WongKe <wong.ke@husky.neu.edu>
2017-04-24 18:44:57 -0400

    Update ShiftLeft2.v

--- ShiftLeft2.v
+++ ShiftLeft2.v
@@ -7,13 +7,13 @@
 
 `timescale 1ns/1ns
 
-module ShiftLeft2(in1, out1);
+module ShiftLeft2(in, out);
   
-  input [31:0] in1;       // output of sign-extend
-  output reg [31:0] out1; // output of shift-left 2
+  input [31:0] in;       // output of sign-extend
+  output reg [31:0] out; // output of shift-left 2
   
-  always @ (in1) begin
+  always @ (in) begin
     // ignore first two bits and add two zeros to end
-    out1 = {in1[29:0], 2'b00};
+    out = {in[29:0], 2'b00};
   end
 endmodule  
