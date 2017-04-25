a492292ceff2bf8d2af5a9e829eef9b317770689
jonm2012 <jonm2012@gmail.com>
2017-04-25 10:49:25 -0400

    Update adder.v

--- adder.v
+++ adder.v
@@ -3,12 +3,13 @@
 // Final Project
 // Kevin Wong & Jonathan Marrero
 
-// 32-bit adder
+// Module for 32 bit adder.
 
 module Adder(in0, in1, out);
-  input [31:0] in0, in1;
-  output reg [31:0] out;
+  input [31:0] in0, in1;    // Input [31-0]
+  output reg [31:0] out;    // Output [1-0]
   
+  // Adder Operation
   always @ (in0 or in1) begin
     out = in0 + in1;
   end
