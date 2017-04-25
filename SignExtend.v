9eef243e14c04a13e675243818d0747bd05f48b5
jonm2012 <jonm2012@gmail.com>
2017-04-25 10:45:09 -0400

    Update SignExtend.v

--- SignExtend.v
+++ SignExtend.v
@@ -3,14 +3,15 @@
 // Final Project
 // Kevin Wong & Jonathan Marrero
 
-// Sign Extend module
+// Module for Sign Extend
+// Input: Instruction [15-0]
+// Output: Sign Extend output
 
 module SignExtend(instr, SEout);
   
-  input [15:0] instr;       // Instruction [15-0]
-  output reg [31:0] SEout; // Sign extend output [31-0]
-  
-  // extend the MSB
+  input [15:0] instr;       // INSTRUCTIONS
+  output reg [31:0] SEout;  // Output of Sign Extend module [31-0]
+  // Sign Extend Operation
   always @ (instr) begin
     SEout <= $signed(instr);
   end
