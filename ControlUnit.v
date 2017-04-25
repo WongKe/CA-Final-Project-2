b38381d59c8df514380ff8112e27061b964faf3a
jonm2012 <jonm2012@gmail.com>
2017-04-25 10:31:40 -0400

    Update ControlUnit.v

--- ControlUnit.v
+++ ControlUnit.v
@@ -3,11 +3,10 @@
 // Final Project
 // Kevin Wong & Jonathan Marrero
 
-// The control unit that takes 6-bit opcode in and generate a set of 9 control signals, 
-// including RegDst, Jump, Branch, MemRead, MemtoReg, MemWrite, ALUsrc, RegWrite, 
-// ALUop (which will be an input to the small ALU Control unit). 
-
-// Taken from Figure 4.22 (Pg. 327)
+// Module for Control Unit
+// Input: 6-bit opcode
+// Output: 9 control signals
+// Control Unit modeled after Pg. 327 Figure 4.22
 
 `timescale 1ns / 1ns
 
@@ -21,7 +20,7 @@ module ControlUnit(opcode, rst, RegDst, Jump, Branch,
               MemWrite, ALUsrc, RegWrite, addi, HLT;
   output reg [1:0] ALUop;
   
-  // initialize
+  // Initialize Control Unit
   initial begin
     RegDst = 0;
     ALUsrc = 0;
@@ -36,12 +35,12 @@ module ControlUnit(opcode, rst, RegDst, Jump, Branch,
     HLT = 0;
   end
         
-  // get outputs
+  // Fetch Outputs
   always @ (opcode) begin
     
-	  // generate control signals
+	  // Control Signals Produced
 	  case (opcode)
-	    // R-format
+	    // R type
 	    6'b000000: begin
 	      RegDst = 1;
 	      ALUsrc = 0;
@@ -54,11 +53,10 @@ module ControlUnit(opcode, rst, RegDst, Jump, Branch,
 	      Jump = 0;
 	      addi = 0;
 	      HLT = 0;
-      end
-      
-      // addi
-      6'b001000: begin
-        RegDst = 0;
+            end
+             // ADDI
+            6'b001000: begin
+              RegDst = 0;
 	      ALUsrc = 1;
 	      MemtoReg = 0;
 	      RegWrite = 1;
@@ -69,83 +67,53 @@ module ControlUnit(opcode, rst, RegDst, Jump, Branch,
 	      Jump = 0;
 	      addi = 1;
 	      HLT = 0;
-      end
-        
-      // lw
-      6'b100011: begin
-        RegDst = 0;
-        ALUsrc = 1;
-        MemtoReg = 1;
-        RegWrite = 1;
-        MemRead = 1;
-        MemWrite = 0;
-        Branch = 0;
+            end
+             // LW
+            6'b100011: begin
+              RegDst = 0;
+              ALUsrc = 1;
+              MemtoReg = 1;
+              RegWrite = 1;
+              MemRead = 1;
+              MemWrite = 0;
+              Branch = 0;
 	      ALUop = 2'b00;
 	      Jump = 0;
 	      addi = 0;
 	      HLT = 0;
-      end
-        
-      // sw
-      6'b101011: begin
-        //RegDst = x;   (don't care)
-        ALUsrc = 1;
-        //MemtoReg = x; (don't care)
-        RegWrite = 0;
-        MemRead = 0;
-        MemWrite = 1;
-        Branch = 0;
+            end
+             // SW
+            6'b101011: begin
+              ALUsrc = 1;
+              RegWrite = 0;
+              MemRead = 0;
+              MemWrite = 1;
+              Branch = 0;
 	      ALUop = 2'b00;
 	      Jump = 0;
 	      addi = 0;
 	      HLT = 0;
-		  end
-        
-      // beq
-      6'b000100: begin
-        //RegDst = x;   (don't care)
-        ALUsrc = 0;
-        //MemtoReg = x; (don't care)
-        RegWrite = 0;
-        MemRead = 0;
-        MemWrite = 0;
-        Branch = 1;
+            end
+             // BEQ
+            6'b000100: begin
+              ALUsrc = 0;
+              RegWrite = 0;
+              MemRead = 0;
+              MemWrite = 0;
+              Branch = 1;
 	      ALUop = 2'b01;
 	      Jump = 0;
 	      addi = 0;
 	      HLT = 0;
-      end
-      
-      // jump
-      6'b000010: begin
-        //RegDst = x;     (don't care)
-        //ALUsrc = x;     (don't care)
-        //MemtoReg = x;   (don't care)
-        //RegWrite = x;   (don't care)
-        //MemRead = x;    (don't care)
-        //MemWrite = x;   (don't care)
-        //Branch = x;     (don't care)
-        //ALUop = 2'bxx;  (don't care)
-        Jump = 1;
-        //addi	= x;       (don't care)
-        //HLT = x;        (don't care)
-      end
-      
-      // Halt
-      6'b111111: begin
-        //RegDst = x;     (don't care)
-        //ALUsrc = x;     (don't care)
-        //MemtoReg = x;   (don't care)
-        //RegWrite = x;   (don't care)
-        //MemRead = x;    (don't care)
-        //MemWrite = x;   (don't care)
-        //Branch = x;     (don't care)
-        //ALUop = 2'bxx;  (don't care)
-        //Jump = x;       (don't care)
-        //addi	= x;       (don't care)
-        HLT = 1;
-      end     
-      
+            end
+             // JUMP
+            6'b000010: begin
+              Jump = 1;
+            end
+             // Halt
+            6'b111111: begin
+              HLT = 1;
+            end     
       default: begin
         RegDst = 0;
         ALUsrc = 0;
@@ -156,7 +124,7 @@ module ControlUnit(opcode, rst, RegDst, Jump, Branch,
         Branch = 0;
         ALUop = 2'b00;
         Jump = 0;
-        addi	= 0;
+        addi = 0;
         HLT = 0;
       end
     endcase
