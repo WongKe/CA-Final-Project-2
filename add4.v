cc80340b1ec9551fddb5b145a296ea7150c763d0
jonm2012 <jonm2012@gmail.com>
2017-04-25 10:47:35 -0400

    Update add4.v

--- add4.v
+++ add4.v
@@ -3,12 +3,14 @@
 // Final Project
 // Kevin Wong & Jonathan Marrero
 
-// adds 4 to the input
+// Module for Add4. 
+// Adds 4 to input.
 
 module Add4(in, out);
-  input [31:0] in;
-  output reg [31:0] out;
+  input [31:0] in;          // Input [31-0]
+  output reg [31:0] out;    // Output [31-0]
   
+  // Add 4 operation
   always @ (in) begin
     out = in + 4;
   end
