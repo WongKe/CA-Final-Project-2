041700c13c03a2f6e6b783f4c487a2651dec25d7
jonm2012 <jonm2012@gmail.com>
2017-04-25 09:55:56 -0400

    Update ALU.v

--- ALU.v
+++ ALU.v
@@ -1,13 +1,11 @@
-// EECE3324 
-// Computer Architecture
-// Final Project
-// Kevin Wong & Jonathan Marrero
+//  EECE3324 
+//  Computer Architecture
+//  Final Project
+//  Kevin Wong & Jonathan Marrero
 
-// An ALU that support a set of MIPS instructions: add, addi, beq, j, lw and sw. The ALU 
-// takes a 4-bit control signal and two 32-bit data inputs in, and generates two outputs 
-// (32-bit ALUresult and 1-bit Zero). 
- 
- 
+//  This module serves as the ALU. Supports the following MIPS instructions:
+//  add, addi, beq, j, lw and sw. Inputs: 4-bit control signal, two 32-bit data inputs.
+//  Outputs: 32-bit ALUresult and 1 bit Zero. 
 
 `timescale 1ns / 1ns
 
@@ -18,26 +16,27 @@ module ALU(ALUctrlop, in0, in1, ALUresult, Zero);
   output reg [31:0] ALUresult;
   output Zero;
   
-  // set zero 
+  // Zero initialized
   assign Zero = (ALUresult == '0);
   
-  // generate ALU result
+  // ALU result produced
  always @ (in0 or in1 or ALUctrlop) begin
     
   case (ALUctrlop)
-      // and
+      // AND
       4'b0000: begin ALUresult = in0 & in1; end
-      // or
+      // OR
       4'b0001: begin ALUresult = in0 | in1; end
-      // add
+      // ADD
       4'b0010: begin ALUresult = in0 + in1; end
-      // sub
+      // SUB
       4'b0110: begin ALUresult = in0 - in1; end
-      // slt
+      // SLT
       4'b0111: begin ALUresult = in0 < in1 ? 1 : 0; end
-      // nor
+      // NOR
      4'b1000: begin ALUresult = ~(in0 | in1); end
-      default: begin ALUresult = 0; end
+      default: begin ALUresult = 0; 
+      end
     endcase
   end
   
