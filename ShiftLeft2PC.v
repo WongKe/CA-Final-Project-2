0bb166967c914358bbb703dc47838736ae304605
jonm2012 <jonm2012@gmail.com>
2017-04-25 10:43:05 -0400

    Update ShiftLeft2PC.v

--- ShiftLeft2PC.v
+++ ShiftLeft2PC.v
@@ -3,24 +3,23 @@
 // Final Project
 // Kevin Wong & Jonathan Marrero
 
-// Shift Left 2 module (PC Loop)
-//
-// The jump target address is obtained by shifting the lower 
-// 26 bits of the jump instruction left 2 bits, and then 
-// concatenating the upper 4 bits of PC+4 as the high-order 
-// bits leading to a 32-bit address.
+// Module for ShiftLeft2PC
+
+// Jump address target is the result of shifting the lower 26 bits of jump instruction
+// left by 2 bits. Concatenate the upper 4 bits of PC+4.
+// Result is 32-bit address
+
 
 `timescale 1ns/1ns
 
 module ShiftLeft2PC(instr, PC4, out);
   
-  input [25:0] instr;       // instruction [25-0]
-  input [31:0] PC4;         // PC+4 bits 
-  output reg [31:0] out;   // output [31-0]
-  
+  input [25:0] instr;         // Instrution [25-0]
+  input [31:0] PC4;           // Output of PC+4 taken as input 
+  output reg [31:0] out;      // ShiftLeft2PC Output [31-0]  
+  // At the end add two zeros to shift left by 2 bits.
+  // Add the upper 4-bits of PC+4
   always @ (instr) begin
-    // add two zeros to end to shift left 2 bits,
-    // then add the upper 4 bits of PC+4
     out = {PC4[31:28], {instr[25:0], 2'b00}}; 
   end
 endmodule  
