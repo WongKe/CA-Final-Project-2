de2cdd946660fb1b648d9d5b2c14a84acad5d988
jonm2012 <jonm2012@gmail.com>
2017-04-25 09:46:52 -0400

    Update 2to1mux.v

--- 2to1mux.v
+++ 2to1mux.v
@@ -1,32 +1,32 @@
-// EECE3324 
-// Computer Architecture
-// Final Project
-// Kevin Wong & Jonathan Marrero
+//  EECE3324 
+//  Computer Architecture
+//  Final Project
+//  Kevin Wong & Jonathan Marrero
 
-// Module for the 2-1 Multiplexer (32 bit)
+//  This module serves as the 2-1 Multiplexer (32 bit)
 
 `timescale 1ns / 1ns
 
 module mux2to1(in0, in1, sel, out);
   
-  input [31:0] in0, in1;    // Input 1 & 2
-  input sel;                // Bit for Select
-  output reg [31:0] out;    // Output
+  input [31:0] in0, in1;    //  Input 1 & 2
+  input sel;                //  Bit for Select
+  output reg [31:0] out;    //  Output
   
   
-  //Loop for MUX operation
+  //  Loop for MUX operation
   
   always @ (sel or in0 or in1)
   begin
     case(sel)
-      // if sel is 0...
+      //  Select = 0
       1'b0: begin
-        // select input 1
+        // Input 1 is chosen
         out <= in0;
       end
-      // if sel is 1...
+      //  Select = 1
       1'b1: begin
-        // select input 2
+        //  Input 2 is chosen
         out <= in1;
       end
     endcase
